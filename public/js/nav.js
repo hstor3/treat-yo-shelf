6142ebb2f2cf656838b5f4a6453bdc8958df3658
Jon Yeatman <jonlyeatman@gmail.com>
2021-05-04 16:19:35 -0400

    Hamburger menu display

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,6 +1,14 @@
-const hamburger = document.getElementById('hamburger');
-const navList = document.getElementById('nav-list');
+let hamburger = document.getElementById('hamburger');
+let navList = document.getElementById('nav-list');
 
-hamburger.addEventListener('click', () => {
-    navList.classList.toggle('show');
-});
+
+hamburger.addEventListener('click', openHam);
+
+// function to open hamburger menu
+function openHam() {
+    navList.style.display = 'flex';
+}
+
+// hamburger.addEventListener('click', () => {
+//     navList.classList.toggle('show');
+// });
