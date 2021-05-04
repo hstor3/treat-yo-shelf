98cb4bb02db44083def7b62030ea5a6f437f3c33
Jon Yeatman <jonlyeatman@gmail.com>
2021-05-04 19:34:35 -0400

    awwwwww

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,14 +1,22 @@
+// let hamburger = document.getElementById('hamburger');
+// let navList = document.getElementById('nav-list');
+
+
+// hamburger.addEventListener('click', openHam);
+
+// // function to open hamburger menu
+// function openHam() {
+//     navList.style.display = 'flex';
+// }
+
 let hamburger = document.getElementById('hamburger');
+if (hamburger) {
+    hamburger.addEventListener('click', openHam)
+    console.log("eeeee");
+}
+
 let navList = document.getElementById('nav-list');
-
-
-hamburger.addEventListener('click', openHam);
-
 // function to open hamburger menu
 function openHam() {
     navList.style.display = 'flex';
 }
-
-// hamburger.addEventListener('click', () => {
-//     navList.classList.toggle('show');
-// });
