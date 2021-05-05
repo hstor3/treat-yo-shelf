922ff9c658828b4dd45ac98457c926fa5c9654ed
Jon Yeatman <jonlyeatman@gmail.com>
2021-05-04 22:48:35 -0400

    lot of animation

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,5 +1,8 @@
+gsap.fromTo(".top-logout-btn", { opacity: 0, scale: 0 },
+    { opacity: 1, scale: 1, duration: 2.5 });
+// home pg
 gsap.fromTo(".logo-home", { opacity: 0, y: -25, scale: 0 },
-    { opacity: 1, y: 0, scale: 1, duration: 2.5 });
+    { opacity: 1, y: 0, scale: 0, duration: 2.5 });
 
 gsap.fromTo(".header-text", { opacity: 0, y: -25, scale: 0 },
     { opacity: 1, y: 0, scale: 1, duration: 2.5 });
@@ -9,3 +12,21 @@ gsap.fromTo(".home-login-card", { opacity: 0, y: 100, scale: 0 },
 
 gsap.fromTo(".home-card", { opacity: 0, y: 100, scale: 0 },
     { opacity: 1, y: 0, scale: 1, duration: 2 });
+
+// login pg
+gsap.fromTo(".logo-login", { opacity: 0, y: -25, scale: 0 },
+    { opacity: 1, y: 0, scale: 1, duration: 2.5 });
+
+gsap.fromTo(".header-text-login, .login-form, .signup-form", { opacity: 0, y: 50 },
+    { opacity: 1, y: 0, duration: 2.5 });
+
+// search 
+gsap.fromTo(".searchbar, .header-text-search", { opacity: 0, y: -25, scale: 0 },
+    { opacity: 1, y: 0, scale: 1, duration: 2.5 });
+
+gsap.fromTo(".searchCard", { opacity: 0, y: 50, scale: 0 },
+    { opacity: 1, y: 0, scale: 1, duration: 2.5 });
+
+// list
+
+// Recommended 
