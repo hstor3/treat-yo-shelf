36d56e86555f4d562c038b98c63241cac1dfb5e9
Heather Storseth <heatherstorseth3@gmail.com>
2021-05-04 21:06:20 -0400

    working on routes

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,24 +1,24 @@
-// const createComment = async (e) => {
-//     e.preventDefault();
+const createComment = async (e) => {
+    e.preventDefault();
 
 //     // add a query selector
-//     const postId = document.querySelector('').value.trim();
-//     conse commentBody = document.querySelector('').value.trim();
+    const postId = document.querySelector('').value.trim();
+    const commentBody = document.querySelector('').value.trim();
 
-//     if (commentBody) {
-//         const response = await fetch(`/api/posts/${id}/comment`, {
-//             method: 'POST',
-//             body: JSON.stringify({ body }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
+    if (commentBody) {
+        const response = await fetch(`/api/posts/${id}/comment`, {
+            method: 'POST',
+            body: JSON.stringify({ body }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-//         if (response.ok) {
-//             document.location.reload()
-//         }
-//     }
-// };
+        if (response.ok) {
+            document.location.reload()
+        }
+    }
+};
 
-// let commentButton = document.querySelector('');
-// if (commentBody) {
-//     commentButton.addEventListener('click', createComment)
-// }
+let commentButton = document.querySelector('');
+if (commentButton) {
+    commentButton.addEventListener('click', createComment)
+}
---
+++ public/js/recommended.js
@@ -0,0 +1,12 @@
+function getApi(genre) {
+    let urlRequest = 'https://openlibrary.org/search.json?q=' + genre
+
+    fetch(urlRequest).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+        let genre = $('<div>').text(data.docs[0].title);
+
+        $('.suggested').append(genre)
+    })
+}
