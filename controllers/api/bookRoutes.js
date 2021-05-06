36610116f8e0bb3343d750d6b253576308767e83
Heather Storseth <heatherstorseth3@gmail.com>
2021-05-06 18:41:09 -0400

    books save to database

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -2,84 +2,53 @@ const router = require("express").Router();
 const { Book } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-// router.get('/search/:id/comments', withAuth, (req, res) => {
-//   res.render('lists', {
-//     loggedIn: req.session.loggedIn
-//   })
-// });
-
-// router.get('search/comments/:id', async (req, res) => {
-//   const commentContent = await Comment.findByPk(req.params.id, {
-//     include: [
-//       {
-//         model: Book,
-//         attributes: ['id'],
-//       },
-//     ],
-//   });
-//   const comments = commentContent.get({ plain: true });
-//   res.render('lists', {
-//     comments,
-//     loggedIn: req.session.loggedIn
-//   })
-// });
+router.get('/books', async (req, res) => {
+  // req.session.userId
+  let booksresult = await Book.findByPk(req.params.id, {
+    include: [
+      {
+        model: Book,
+        attributes: ['id'],
+      },
+    ],
+  });
+  const books = booksresult.get({ plain: true });
+  res.render('lists', {
+    books,
+    loggedIn: req.session.loggedIn
+  })
+});
 
 router.post('/', (req, res) => {
-  console.log(req);
+  // console.log(req);
   Book.create({
     title: req.body.title,
     author: req.body.author
   }).then((addBook) => {
     res.status(200).json(addBook)
   })
-  //   include: [
-  //     {
-  //       model: Book,
-  //       attributes: ['id'],
-  //     },
-  //   ],
-  // });
-  // const book = bookData.get({ plain: true });
-  // res.render('lists', {
-  //   book,
-  //   loggedIn: req.session.loggedIn
-  // })
 });
 
-// router.post('/', (req, res) => {
-//   Comment.create({
-//     body: req.body.body,
-//     title: req.body.title,
-//     userId: req.session.userId
-//   }).then((createPost) => {
-//     res.status(200).json(createPost);
-//   })
-// });
+router.put('/:id', (req, res) => {
+  Book.update(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  }).then(newBook => {
+    res.status(200).json(newBook)
+  })
+  res.render('lists')
+})
 
-// router.put("/:id", (req, res) => {
-//   Post.update(req.body, {
-//     where: {
-//       id: req.params.id
-//     }
-//   }).then(updatePost => {
-//       res.status(200).json(updatePost)
-//     })
-// });
-
-// router.put('/', (req, res) => {
-//   res.redirect('/posts');
-//   return;
-// });
-
-// router.delete('/:id', (req, res) => {
-//     Post.destroy({
-//       where: {
-//         id: req.params.id,
-//       },
-//     }).then(deletePost => {
-//       res.status(200).json(deletePost)
-//       res.render('/')
-//     })
-// });
+router.delete('/:id', (req, res) => {
+  Book.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(removeBook => {
+    res.status(200).json(removeBook)
+    res.render('lists')
+  })
+})
 
 module.exports = router;
