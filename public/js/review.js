086e35cc61f912b8f3769539235ba96f8ad7c044
Heather Storseth <heatherstorseth3@gmail.com>
2021-05-07 12:55:13 -0400

    reviews save to database

--- public/js/review.js
+++ public/js/review.js
@@ -1,22 +1,22 @@
-const newReview = async (e) => {
-    e.preventDefault();
-    console.log('does this work?')
+// const newReview = async (e) => {
+//     e.preventDefault();
+//     console.log('does this work?')
 
-    const id = e.target.getAttribute('data-id');
-    const body = document.querySelector('.review-input').value.trim();
+//     const id = e.target.getAttribute('data-id');
+//     const body = document.querySelector('.review-input').value.trim();
 
-    if (body) {
-        const response = await fetch(`/api/books/${id}/reviews`, {
-            method: 'POST',
-            body: JSON.stringify({ body }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            console.log('comments work!!')
-            document.location.reload()
-        }
-    }
-};
+//     if (body) {
+//         const response = await fetch(`/api/books/${id}/reviews`, {
+//             method: 'POST',
+//             body: JSON.stringify({ body }),
+//             headers: { 'Content-Type': 'application/json' },
+//         });
+//         if (response.ok) {
+//             console.log('comments work!!')
+//             document.location.reload()
+//         }
+//     }
+// };
 
 // let confirmReview = document.getElementById('confirm-review');
 // if (confirmReview) {
